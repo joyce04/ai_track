26139d0a400ca9f10281d715642e0f8515b4ffe0
grace lee <grace@graces-MacBook-Pro.local>
2018-01-11 08:04:03 +0900

    practice insertion sort

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.0.1",
     author = "Grace Lee",
     description = (""),
-    packages=['practice', 'test'],
+    packages=['practice', 'test', 'algorithm'],
     #classifiers=[
     #    "Development Status :: 3 - Alpha",
     #    "Topic :: Utilities",
